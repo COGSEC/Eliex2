b5d941f6a4a9284cfb9bf18fdd2e5effc4eb5e9f
Cordes-RJ <richardj.cordes@gmail.com>
2021-01-11 16:24:31 -0500

    added function to hash strings

--- src/utilHash.py
+++ src/utilHash.py
@@ -8,6 +8,8 @@ def getMD5(filepath):
         data = file.read()
     return hashlib.md5(data).hexdigest()
     
+def createMD5(data):
+    return hashlib.md5(data.encode('utf-8')).hexdigest()
 """
 x = getMD5('config.ini') out: de88ec9491a8b87697751db5cc5b2fd1
 """
