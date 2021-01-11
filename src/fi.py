557f7df2274097fd27b9008de9fe319c15b61d50
Cordes-RJ <richardj.cordes@gmail.com>
2021-01-11 15:45:24 -0500

    fileinfo object working

---
+++ src/fi.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from utilFile import getFileName
+from utilHash import getMD5
+from coms import ASKforShortTitle
+from coms import NOTIFYforHashHandling
+
+class fileInfo:
+    def __init__(self, path):
+        # stage 1 items
+        self.path = path
+        self.filename = getFileName(path)
+        self.hash = getMD5(path)
+        # stage 2 items        
+        self.bloc = ""
+        # stage 3 items
+        self.shortTitle = ""
+    def ASKforShortTitle(self):
+        self.shortTitle = ASKforShortTitle(self.filename)
+    def NOTIFYforHashHandling(self):
+        NOTIFYforHashHandling(self.filename, self.hash)
+    def SetBloc(self, bloc):
+        self.bloc = bloc
+    def GetHash(self):
+        return self.hash
+    def GetPath(self):
+        return self.path
+    def GetFileName(self):
+        return self.filename
+    def GetShortTitle(self):
+        return self.shortTitle
