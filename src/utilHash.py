506482693207fb11b32aaf5636362f2b5f9422f6
Cordes-RJ <richardj.cordes@gmail.com>
2021-01-11 15:25:32 -0500

    utilHash added

---
+++ src/utilHash.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import hashlib
+
+# getMD5 returns an MD5 hash of a file, will return "" on error.
+def getMD5(filepath):
+    with open(filepath, 'rb') as file:
+        data = file.read()
+    return hashlib.md5(data).hexdigest()
+    
+"""
+x = getMD5('config.ini') out: de88ec9491a8b87697751db5cc5b2fd1
+"""
