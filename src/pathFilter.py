af919b8cd34f694476da35c329bbef57455e9d02
Cordes-RJ <richardj.cordes@gmail.com>
2021-01-11 20:00:19 -0500

    pathFilter working

---
+++ src/pathFilter.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+import utilFile
+from utilMisc import csvStringToList
+from coms import NOTIFYduplicateFound
+
+class Filter:
+    def __init__(self, configMngr):
+        self.failLoc = configMngr.GetPath('FailedImport')
+        self.hLibLoc =configMngr.GetPath('HashLib')
+        self.ignoreList = csvStringToList(configMngr.Get('IgnoreList'))
+        self.blackList = csvStringToList(configMngr.Get('BlackList'))
+    def Scrub(self, fiList):
+        return self.checkDuplicates(self.checkBlack(self.checkIgnore(fiList)))
+    def checkIgnore(self,fiList):
+        i = 0
+        while i < len(fiList):
+            mark = True
+            for item in self.ignoreList:
+                if item in fiList[i].GetFileName():
+                    del fiList[i]
+                    mark = False
+                    break
+            if mark:
+                i+=1
+        return fiList
+    def checkBlack(self,fiList):
+        i = 0
+        while i < len(fiList):
+            mark = True
+            for item in self.blackList:
+                if item in fiList[i].GetFileName():
+                    fiList[i].MoveToFolder(self.failLoc)
+                    del fiList[i]
+                    mark = False
+                    break
+            if mark:
+                i+=1
+        return fiList
+    def checkDuplicates(self,fiList):
+        i = 0
+        while i < len(fiList):
+            mark = True
+            if utilFile.checkForFile(self.hLibLoc+"/"+fiList[i].GetHash()):
+                NOTIFYduplicateFound(fiList[i].GetHash(),fiList[i].GetFileName())
+                fiList[i].MoveToFolder(self.failLoc)
+                del fiList[i]
+                mark = False
+            if mark:
+                i+=1
+        return fiList
+    
