62b7a3d780cfff6905fb472f0f1626b3fcf2123a
Cordes-RJ <richardj.cordes@gmail.com>
2021-01-11 22:21:55 -0500

    added archive functions for librarian to call

--- src/fi.py
+++ src/fi.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from utilFile import makeFile
+from utilFile import makeFullPath
 from utilFile import getFileName
 from utilHash import getMD5
 from utilFile import moveToFolder
@@ -35,3 +37,18 @@ class fileInfo:
         moveToFolder(self.path,folder)
     def RenameAndMove(self, folder):
         renameAndMove(self.path,folder,self.shortTitle)
+    def writeReportString(self):
+        rs = ""
+        rs += self.hash +", "
+        rs += self.filename
+        return rs
+    def makeHashFile(self,hashLib):
+        makeFile(makeFullPath(hashLib,self.hash),"")
+    def makeMDFile(self,vault):
+        makeFile(makeFullPath(vault,self.shortTitle+".md"),self.bloc)
+    def Archive(self,vault,doclib,hashlib):
+        self.makeMDfile(vault)
+        self.makeHashFile(hashlib)
+        self.RenameAndMove(doclib)
+        
+        
