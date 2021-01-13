f71cd5e743d4318f76fc0348a3d01b29a3144199
Cordes-RJ <richardj.cordes@gmail.com>
2021-01-13 10:17:30 -0500

    bugfix, catch permissionerror

--- src/utilHash.py
+++ src/utilHash.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
 
 import hashlib
+import os
 
 # getMD5 returns an MD5 hash of a file, will return "" on error.
 def getMD5(filepath):
-    with open(filepath, 'rb') as file:
-        data = file.read()
-    return hashlib.md5(data).hexdigest()
+    try:
+        with open(filepath, 'rb') as file:
+            data = file.read()
+        return hashlib.md5(data).hexdigest()
+    except PermissionError:
+        print("Received PermissionError!")
+        cmd = input("Type close to close program, or hit enter to try again")
+        if cmd == "close":
+            os.exit(1)
+        else:
+            getMD5(filepath)
     
 def createMD5(data):
     return hashlib.md5(data.encode('utf-8')).hexdigest()
